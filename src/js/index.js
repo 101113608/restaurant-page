f60e765a73f1099d2163152d6caacb3557e6a14a
101113608 <48575130+101113608@users.noreply.github.com>
2025-02-16 04:35:32 +1100

    Update index.js to generate the 'home' page
    To generate the contents of the 'home' page via the imported function
    from home.js, index.js has been updated to now append the result from
    the imported function to the DOM. Now, when the website loads, the
    'home' page content is automatically generated and displayed when
    index.js runs.
    
    Consequently, the template.html has been updated to remove the temporary
    HTML that was there previously for the purposes of styling the look
    of the 'home' page.

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,18 @@
 import "../styles.css";
 import { generateHomeContent } from "./home.js";
+
+const contentDiv = document.querySelector("#content");
+
+function displayContent(tabName, generateContentFn) {
+    contentDiv.textContent = "";
+    contentDiv.removeAttribute("class");
+
+    contentDiv.classList.add(tabName);
+    contentDiv.append(generateContentFn());
+}
+
+function initContent() {
+    displayContent("home", generateHomeContent);
+}
+
+initContent();
