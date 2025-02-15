4341dbf06ed8a76633e4532d3d2d295c415683ef
101113608 <48575130+101113608@users.noreply.github.com>
2025-02-16 04:15:40 +1100

    Create home.js script and add to 'js' folder
    To generate the content for the 'home' page dynamically, the 'home.js'
    script was created. The aim is for the index.js file to import a
    function from 'home.js' that will generate all of the necessary
    content for displaying the 'home' page when said function is run.

---
+++ src/js/home.js
@@ -0,0 +1,95 @@
+class SubsectionItem {
+    constructor(element, text) {
+        this.element = element;
+        this.text = text;
+    }
+};
+
+const tradingHoursSubsection = [
+    new SubsectionItem(
+        "h2",
+        "Trading Hours",
+    ),
+    new SubsectionItem(
+        "ul",
+        [
+            "Monday: Closed",
+            "Tuesday - Friday: 11:00 - 22:00",
+            "Saturday - Sunday: 17:00 - 01:00",
+        ]
+    ),
+];
+
+const addressSubsection = [
+    new SubsectionItem(
+        "h2",
+        "Location",
+    ),
+    new SubsectionItem(
+        "ul",
+        [
+            "123 Seaside Avenue",
+            "Atlantis City",
+            "Planet Earth",
+        ]
+    ),
+];
+
+const descriptionSubsection = [
+    new SubsectionItem(
+        "h1",
+        "A Bowl of Happiness Awaits!"
+    ),
+    new SubsectionItem(
+        "p",
+        "A new Japanese Ramen Store located on planet Earth,come and enjoy a delicious bowl of ramen at deliciously low prices!"
+    ),
+    new SubsectionItem(
+        "button",
+        "View Menu"
+    ),
+];
+
+function createListElement(subsectionItem) {
+    const listElement = document.createElement(`${subsectionItem.element}`);
+    subsectionItem.text.forEach(string => {
+        const listItemElement = document.createElement("li");
+        listItemElement.textContent = string;
+        listElement.append(listItemElement);
+    });
+
+    return listElement;
+};
+
+function createSubsection(subsection) {
+    const subsectionDiv = document.createElement("div");
+    subsection.forEach(subsectionItem => {
+        let element;
+        if (subsectionItem.element === "ul" && Array.isArray(subsectionItem.text)) {
+            element = createListElement(subsectionItem);
+        } else {
+            element = document.createElement(`${subsectionItem.element}`)
+            element.textContent = subsectionItem.text;
+        }
+        subsectionDiv.append(element);
+    })
+
+    subsectionDiv.classList.add("subsection");
+
+    return subsectionDiv;
+};
+
+function generateHomeContent() {
+    const divElement = document.createElement("div");
+    const descriptionElement = createSubsection(descriptionSubsection);
+    const tradingHoursElement = createSubsection(tradingHoursSubsection);
+    const addressElement = createSubsection(addressSubsection);
+
+    divElement.classList.add("section");
+
+    divElement.append(descriptionElement, tradingHoursElement, addressElement);
+
+    return divElement;
+};
+
+export { generateHomeContent };
