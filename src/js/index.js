a13ef3909bd74ec476a866f44dac0c0c073b3566
101113608 <48575130+101113608@users.noreply.github.com>
2025-02-17 23:30:09 +1100

    Update index.js to generate 'contact us' page
    To generate the contents of the 'contact us' page, index.js was updated
    to import and run a function from contact-us.js that generates the
    contents of said page, and append it to the DOM. For now, when the
    website loads, the 'contact us' page is automatically generated when
    index.js runs.
    
    With this, the template.html has been updated to remove the temporary
    HTML that was there previously for the purposes of styling the look of
    the 'contact us' page.

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,39 @@
 import "../styles.css";
 import { generateHomeContent } from "./home.js";
+import { generateContactContent } from "./contact-us.js";
 
 const contentDiv = document.querySelector("#content");
+
+function appendToContentDiv(content) {
+    if (Array.isArray(content)) {
+        content.forEach(section => contentDiv.append(section));
+    } else {
+        contentDiv.append(content);
+    };
+}
+
+function setContentDivClass(tabName) {
+    if (Array.isArray(tabName)) {
+        tabName.forEach(name => contentDiv.classList.add(`${name}`));
+    } else {
+        contentDiv.classList.add(tabName);
+    };
+}
 
 function displayContent(tabName, generateContentFn) {
     contentDiv.textContent = "";
     contentDiv.removeAttribute("class");
 
-    contentDiv.classList.add(tabName);
-    contentDiv.append(generateContentFn());
+    setContentDivClass(tabName);
+
+    const content = generateContentFn();
+    appendToContentDiv(content);
 }
 
-function initContent() {
-    displayContent("home", generateHomeContent);
-}
+displayContent(["banner-page", "contact-us"], generateContactContent);
 
-initContent();
+// function initContent() {
+//     displayContent("home", generateHomeContent);
+// }
+
+// initContent();
