91513e1aae415a0781ab04d9fc9ba9ef75bacd09
101113608 <48575130+101113608@users.noreply.github.com>
2025-02-17 23:40:01 +1100

    Update index.js to switch b/w pages on tab click
    In order to switch between the 'home' and 'contact us' pages, index.js
    has been updated so that clicking on either the 'home' or 'contact us'
    tab in the 'nav' element now generates the contents for that page onto
    the DOM. Note that the 'menu' tab is not yet functional, as the page has
    not been implemented yet.

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,17 @@ import { generateHomeContent } from "./home.js";
 import { generateContactContent } from "./contact-us.js";
 
 const contentDiv = document.querySelector("#content");
+const navMenu = document.querySelector("nav ul");
+
+function highlightCurrentTab(tabName) {
+    const navButtons = navMenu.querySelectorAll("button");
+    navButtons.forEach(button => {
+        button.removeAttribute("class");
+        if (tabName === button.dataset.tab) {
+            button.classList.add("highlight-tab");
+        }
+    });
+}
 
 function appendToContentDiv(content) {
     if (Array.isArray(content)) {
@@ -30,10 +41,27 @@ function displayContent(tabName, generateContentFn) {
     appendToContentDiv(content);
 }
 
-displayContent(["banner-page", "contact-us"], generateContactContent);
+navMenu.addEventListener("click", event => {
+    if (event.target.dataset.tab) {
+        switch (event.target.dataset.tab) {
+            case 'home':
+                displayContent(event.target.dataset.tab, generateHomeContent);
+                break;
+            case 'contact-us':
+                displayContent(["banner-page", event.target.dataset.tab], generateContactContent);
+                document.querySelector("form").addEventListener("submit", () => initContent());
+                break;
+            default:
+                console.error("Error with selecting nav menu item");
+                break;
+        }
+        highlightCurrentTab(event.target.dataset.tab);
+    }
+});
 
-// function initContent() {
-//     displayContent("home", generateHomeContent);
-// }
+function initContent() {
+    displayContent("home", generateHomeContent);
+    highlightCurrentTab("home");
+}
 
-// initContent();
+initContent();
