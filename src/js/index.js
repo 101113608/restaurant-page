fb537a30d4c2a4d82b3cb142b0032cfcc94c4219
101113608 <48575130+101113608@users.noreply.github.com>
2025-02-21 09:16:17 +1100

    Update index.js to generate 'attributions' modal
    To generate the contents of the 'attributions' modal, index.js was
    updated to import and run a function from attributions.js. This
    function generates a modal containing attributions for all the assets
    that were used on the website. The modal will display when clicking on
    the 'attributions/credits' button located on the footer of the website.
    
    With this, the template.html has been updated to remove the temporary
    HTML that was there previously for the purposes of styling the look of
    the 'attributions' modal.

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,12 @@ import "../styles.css";
 import { generateHomeContent } from "./home.js";
 import { generateContactContent } from "./contact-us.js";
 import { generateMenuContent } from "./menu.js";
+import { generateAttributionsModal } from "./attributions.js";
 
 const contentDiv = document.querySelector("#content");
 const navMenu = document.querySelector("nav ul");
+const body = document.querySelector("body");
+const dialog = generateAttributionsModal();
 
 function highlightCurrentTab(tabName) {
     const navButtons = navMenu.querySelectorAll("button");
@@ -70,9 +73,25 @@ contentDiv.addEventListener("click", event => {
     }
 });
 
+body.addEventListener("click", event => {
+    if (event.target.id === "attributions") {
+        body.classList.add("open-modal");
+        dialog.showModal();
+    }
+});
+
+dialog.addEventListener("click", event => {
+    // Clicking outside of the modal's contents (dialog backdrop)
+    if (event.target.id === "modal") {
+        dialog.close();
+        body.classList.remove("open-modal")
+    }
+});
+
 function initContent() {
     displayContent("home", generateHomeContent);
     highlightCurrentTab("home");
+    body.append(dialog);
 }
 
 initContent();
